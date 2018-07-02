e22a4970c70f450984bb277067cafbcf08dcfa3c
Francesco Intini <francesco.intini@student.unife.it>
2018-07-02 19:06:49 +0000

    Update inserimento.js

--- src/inserimento.js
+++ src/inserimento.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     var counter = 0;
 
+
+///funzione da ampliare
     $("#addrow").on("click", function () {
         var newRow = $("<tr>");
         var cols = "";
@@ -38,3 +40,45 @@ function calculateGrandTotal() {
     });
     $("#grandtotal").text(grandTotal.toFixed(2));
 }
+
+//download del file xml da identare
+
+function downloadData(contentType,data,filename){
+	 
+    var link=document.createElement("A");
+    link.setAttribute("href",encodeURI("data:"+contentType+","+data));
+    link.setAttribute("style","display:none");
+    link.setAttribute("download",filename);
+    document.body.appendChild(link); //needed for firefox
+    console.log(link.outerHTML);
+    link.click();
+    setTimeout(function(){
+        document.body.removeChild(link);
+    },1000);
+ }
+ 
+ function fromToXml(form){
+     var xmldata=['<?xml version="1.0"?>'];
+       xmldata.push("<form>");
+     var inputs=form.elements;
+     for(var i=0;i<inputs.length;i++){
+         var el=document.createElement("ELEMENT");
+       if (inputs[i].name){
+           el.setAttribute("name",inputs[i].name);
+         el.setAttribute("value",inputs[i].value);
+         xmldata.push(el.outerHTML);
+       }
+       
+     }
+     xmldata.push("</form>");
+     return xmldata.join("\n");
+ }
+ 
+ 
+ function download(frm){
+ 
+     var data=fromToXml(frm);
+   console.log(data);
+   
+   downloadData("text/xml",data,"export.xml");
+ }
